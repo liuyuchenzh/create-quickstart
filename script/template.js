ff19b96832dacd09e9dc1bba018945d07c446a62
liuyuchen <liuyuchen-iri@360.cn>
2017-07-12 10:49:18 +0800

    remove uesless comment

--- script/template.js
+++ script/template.js
@@ -1,8 +1,11 @@
 function template (content, data) {
   const reg = /\/\*{{(\w+)}}\*\/[\s\S]*?\/\*end\s*{{\1}}\*\//g
-  return content.replace(reg, (match, key) => {
-    return data[key] ? match : ''
-  })
+  return content
+    .replace(reg, (match, key) => {
+      return data[key] ? match : ''
+    })
+    // remove useless comment
+    .replace(/\/\*(end\s*)?{{(\w+)}}\*\//g, '')
 }
 
 module.exports = {
