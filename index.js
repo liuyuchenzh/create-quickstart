68f1688cc4cb7cb46fad61140b5d1f5a0faff18a
Yuchen Liu <liuyuchenzh@163.com>
2018-09-03 18:32:44 +0800

    feat: ready for beta

--- index.js
+++ index.js
@@ -124,120 +124,129 @@ const questions = [
 // install cli and execute
 // else cd to directory
 // copy template then install
-inquirer.prompt(questions).then(answer => {
-  const { directory, framework, typescript, css, installer, registry } = answer;
-  if (!usingVanilla(answer)) {
-    // use official cli
-    const cli = OFFICIAL_CLI[framework];
-    const command = OFFICIAL_CLI_COMMAND[framework];
-    console.log(`installing ${cli}...`);
-    install(installer, cli, "-g");
-    console.log(`using ${cli} to init the project`);
-    execute("npx", command.split(" ").concat(directory));
-    return;
-  }
-  // copy template so it will be left untouched
-  const tempLocation = resolve("temp");
-  fse.copySync(resolve("template"), tempLocation);
-  const indexJsLocation = resolve("temp/src/index.js");
-  const copyList = [
-    "build/",
-    "webpack/",
-    "index.html",
-    "src/",
-    "package.json",
-    "README.md"
-  ];
-  // merge package.json
-  // update package.json
-  // copy eslint/tslint files to template root
-  // copy template to given directory
-  // clean template
-  // cd to given directory
-  // run npm install or yarn
+export default function init() {
+  inquirer.prompt(questions).then(answer => {
+    const {
+      directory,
+      framework,
+      typescript,
+      css,
+      installer,
+      registry
+    } = answer;
+    if (!usingVanilla(answer)) {
+      // use official cli
+      const cli = OFFICIAL_CLI[framework];
+      const command = OFFICIAL_CLI_COMMAND[framework];
+      console.log(`installing ${cli}...`);
+      install(installer, cli, "-g");
+      console.log(`using ${cli} to init the project`);
+      execute("npx", command.split(" ").concat(directory));
+      return;
+    }
+    // copy template so it will be left untouched
+    const tempLocation = resolve("temp");
+    fse.copySync(resolve("template"), tempLocation);
+    const indexJsLocation = resolve("temp/src/index.js");
+    const copyList = [
+      "build/",
+      "webpack/",
+      "index.html",
+      "src/",
+      "package.json",
+      "README.md"
+    ];
+    // merge package.json
+    // update package.json
+    // copy eslint/tslint files to template root
+    // copy template to given directory
+    // clean template
+    // cd to given directory
+    // run npm install or yarn
 
-  // use typescript or javascript
-  let scriptCopyLocation = resolve("template/jsconfig/copy/");
-  if (typescript) {
-    const tsPackage = require("./template/tsconfig/package.json");
-    packageJson = merge(packageJson, tsPackage);
-    scriptCopyLocation = resolve("template/tsconfig/copy/");
-    // update jsloader.json
-    const loader = require("./template/webpack/loader/jsloader.json");
-    loader.test = "\\.tsx?$";
-    loader.use = "ts-loader";
-    loader.ext = "ts";
-    const jsloaderLocation = resolve("temp/webpack/loader/jsloader.json");
-    write(jsloaderLocation, JSON.stringify(loader, null, 2));
-  } else {
-    const jsPackage = require("./template/jsconfig/package.json");
-    packageJson = merge(packageJson, jsPackage);
-  }
-  // copy lint files etc
-  fse.copySync(scriptCopyLocation, tempLocation);
-  copyList.push(...fs.readdirSync(scriptCopyLocation));
+    // use typescript or javascript
+    let scriptCopyLocation = resolve("template/jsconfig/copy/");
+    if (typescript) {
+      const tsPackage = require("./template/tsconfig/package.json");
+      packageJson = merge(packageJson, tsPackage);
+      scriptCopyLocation = resolve("template/tsconfig/copy/");
+      // update jsloader.json
+      const loader = require("./template/webpack/loader/jsloader.json");
+      loader.test = "\\.tsx?$";
+      loader.use = "ts-loader";
+      loader.ext = "ts";
+      const jsloaderLocation = resolve("temp/webpack/loader/jsloader.json");
+      write(jsloaderLocation, JSON.stringify(loader, null, 2));
+    } else {
+      const jsPackage = require("./template/jsconfig/package.json");
+      packageJson = merge(packageJson, jsPackage);
+    }
+    // copy lint files etc
+    fse.copySync(scriptCopyLocation, tempLocation);
+    copyList.push(...fs.readdirSync(scriptCopyLocation));
 
-  const styleExt = STYLE_EXT[css];
-  // update style file extension
-  // style reference
-  // as well as webpack loader
-  if (css !== "postcss") {
-    // update extension
-    const cssLocation = resolve("temp/src/index.css");
-    const cssLoaderLocation = resolve("temp/webpack/loader/cssloader.json");
-    renameExt("css", styleExt)(cssLocation);
-    // update reference in index.js
-    const indexJs = read(indexJsLocation);
-    const newJsContent = indexJs.replace(/(index)\.css/, `$1.${styleExt}`);
-    write(indexJsLocation, newJsContent);
-    // update loader info
-    const cssLoader = require("./template/webpack/loader/cssloader.json");
-    cssLoader.use = `${css}-loader`;
-    cssLoader.test = `\\.${styleExt}$`;
-    write(cssLoaderLocation, formatJSON(cssLoader));
-  } else {
-    copyList.push("postcss.config.js");
-  }
-  // merge css package.json
-  packageJson = merge(
-    packageJson,
-    require(`./template/cssconfig/package.${css}.json`)
-  );
-  // update package.json
-  write(resolve("temp/package.json"), formatJSON(packageJson));
+    const styleExt = STYLE_EXT[css];
+    // update style file extension
+    // style reference
+    // as well as webpack loader
+    if (css !== "postcss") {
+      // update extension
+      const cssLocation = resolve("temp/src/index.css");
+      const cssLoaderLocation = resolve("temp/webpack/loader/cssloader.json");
+      renameExt("css", styleExt)(cssLocation);
+      // update reference in index.js
+      const indexJs = read(indexJsLocation);
+      const newJsContent = indexJs.replace(/(index)\.css/, `$1.${styleExt}`);
+      write(indexJsLocation, newJsContent);
+      // update loader info
+      const cssLoader = require("./template/webpack/loader/cssloader.json");
+      cssLoader.use = `${css}-loader`;
+      cssLoader.test = `\\.${styleExt}$`;
+      write(cssLoaderLocation, formatJSON(cssLoader));
+    } else {
+      copyList.push("postcss.config.js");
+    }
+    // merge css package.json
+    packageJson = merge(
+      packageJson,
+      require(`./template/cssconfig/package.${css}.json`)
+    );
+    // update package.json
+    write(resolve("temp/package.json"), formatJSON(packageJson));
 
-  // only update .js -> .ts here since css reference may needs to be updated too
-  if (typescript) {
-    renameExt("js", "ts")(indexJsLocation);
-  }
+    // only update .js -> .ts here since css reference may needs to be updated too
+    if (typescript) {
+      renameExt("js", "ts")(indexJsLocation);
+    }
 
-  // create .npmrc
-  if (registry && registry.trim()) {
-    const content =
-      registry.trim().indexOf("registry=") === 0
-        ? registry
-        : `registry=${registry}`;
-    write(resolve("temp/.npmrc"), content);
-    copyList.push(".npmrc");
-  }
-  const ws = process.cwd();
-  const dist = path.resolve(ws, directory || "");
-  // copy files to dist
-  copyList.forEach(file =>
-    fse.copySync(resolve(`temp/${file}`), path.resolve(dist, file))
-  );
-  // clean temp directory
-  rimraf(tempLocation, e => {
-    if (!e) return;
-    console.log(e);
+    // create .npmrc
+    if (registry && registry.trim()) {
+      const content =
+        registry.trim().indexOf("registry=") === 0
+          ? registry
+          : `registry=${registry}`;
+      write(resolve("temp/.npmrc"), content);
+      copyList.push(".npmrc");
+    }
+    const ws = process.cwd();
+    const dist = path.resolve(ws, directory || "");
+    // copy files to dist
+    copyList.forEach(file =>
+      fse.copySync(resolve(`temp/${file}`), path.resolve(dist, file))
+    );
+    // clean temp directory
+    rimraf(tempLocation, e => {
+      if (!e) return;
+      console.log(e);
+    });
+    // cd to dist and install all dependencies
+    process.chdir(dist);
+    const method = installer === "npm" ? "i" : "";
+    console.log("installing...");
+    execute(installer, [method]);
+    console.log("all done");
+    console.log("please enter following command:");
+    console.log(`- cd ${directory}`);
+    console.log(`- npm run start`);
   });
-  // cd to dist and install all dependencies
-  process.chdir(dist);
-  const method = installer === "npm" ? "i" : "";
-  console.log("installing...");
-  execute(installer, [method]);
-  console.log("all done");
-  console.log("please enter following command:");
-  console.log(`- cd ${directory}`);
-  console.log(`- npm run start`);
-});
+}
