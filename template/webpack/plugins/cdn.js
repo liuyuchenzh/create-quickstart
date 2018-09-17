46e39d3a203b5424cb9ecbbb5b6d6cb4847c4a75
Yuchen Liu <liuyuchenzh@163.com>
2018-09-17 13:48:05 +0800

    feat: use concatenation for production

--- template/webpack/plugins/cdn.js
+++ template/webpack/plugins/cdn.js
@@ -1,7 +1,10 @@
+const webpack = require("webpack");
 const pluginJson = require("./plugins.json");
 const { name } = pluginJson.plugins.find(item => item.key === "cdn") || {};
 module.exports = name
   ? [
+      // bring concatenation back
+      new webpack.optimize.ModuleConcatenationPlugin(),
       new require("webpack-upload-plugin")(require(name), {
         enableCache: true
       })
