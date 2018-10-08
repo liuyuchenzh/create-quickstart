92243d648cd758023a3fea6db901ad1aabda0bd9
Yuchen Liu <liuyuchenzh@163.com>
2018-10-08 09:19:21 +0800

    feat: lint-stage && fix: fail to require cdn plugin

--- template/webpack/plugins/cdn.js
+++ template/webpack/plugins/cdn.js
@@ -1,12 +1,17 @@
 const webpack = require("webpack");
 const pluginJson = require("./plugins.json");
 const { name } = pluginJson.plugins.find(item => item.key === "cdn") || {};
-module.exports = name
-  ? [
+module.exports = (function() {
+  if (name) {
+    const CdnPlugin = require("webpack-upload-plugin");
+    const cdn = require(name);
+    return [
       // bring concatenation back
       new webpack.optimize.ModuleConcatenationPlugin(),
-      new require("webpack-upload-plugin")(require(name), {
+      new CdnPlugin(cdn, {
         enableCache: true
       })
-    ]
-  : [];
+    ];
+  }
+  return [];
+})();
