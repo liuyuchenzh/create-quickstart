96be2a05575e4b93edab8b1443a1fda7302ad023
Yuchen Liu <liuyuchenzh@163.com>
2018-11-08 22:14:18 +0800

    fix: handle .gitignore

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function init() {
       "package.json",
       "README.md",
       ".editorconfig",
-      ".gitignore"
+      ".gitignore_keep"
     ];
     // merge package.json
     // update package.json
@@ -281,7 +281,7 @@ function init() {
     const dist = path.resolve(ws, directory || "");
     // copy files to dist
     copyList.forEach(file =>
-      fse.copySync(resolve(`temp/${file}`), path.resolve(dist, file))
+      fse.copySync(resolve(`temp/${file}`), path.resolve(dist, /^\.gitignore/.test(file) ? '.gitignore' : file))
     );
     // clean temp directory and package.jon in webpack/plugins
     const deleteLocations = [
