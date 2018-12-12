063047f634f75e25d51345aecb52596314ba22d1
Yuchen Liu <liuyuchenzh@163.com>
2018-12-12 11:30:53 +0800

    fix: remove extra plugins

--- template/webpack/plugins/cdn.js
+++ template/webpack/plugins/cdn.js
@@ -1,4 +1,3 @@
-const webpack = require("webpack");
 const pluginJson = require("./plugins.json");
 const { name } = pluginJson.plugins.find(item => item.key === "cdn") || {};
 module.exports = (function() {
@@ -6,8 +5,6 @@ module.exports = (function() {
     const CdnPlugin = require("webpack-upload-plugin");
     const cdn = require(name);
     return [
-      // bring concatenation back
-      new webpack.optimize.ModuleConcatenationPlugin(),
       new CdnPlugin(cdn, {
         enableCache: true
       })
